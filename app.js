7ebfcefaa5d983597bcb81df8aa1d412fd71b8f5
Fanta335 <fantayusuke5210@gmail.com>
2021-12-01 15:34:47 +0900

    add favicon

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const PORT = process.env.PORT;
+const path = require("path");
 const express = require("express");
+const favicon = require("serve-favicon");
 const app = express();
 
+// Express settings
 app.set("view engine", "ejs");
 
+// Static resource routing
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+// Dynamic resource routing
 app.use("/", require("./routes/index"));
 
+// Execute web application
 app.listen(PORT, () => {
   console.log(`Application listeng at http://localhost:${PORT}`);
 });
