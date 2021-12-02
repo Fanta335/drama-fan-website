1bcfb714e0919fb6cdfbe13f1505f61deddfc3b1
Fanta335 <fantayusuke5210@gmail.com>
2021-12-02 19:41:43 +0900

    add walter white

--- routes/characters.js
+++ routes/characters.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const { characters } = require("../lib/data/character_list");
 
 router.get("/", (req, res) => {
   res.render("./characters.ejs");
 });
+
+for (let i = 0; i < characters.length; i++) {
+  const curr = characters[i];
+  router.get(`/${curr}`, (req, res) => {
+    res.render(`./characters/${curr}.ejs`);
+  });
+}
 
 module.exports = router;
